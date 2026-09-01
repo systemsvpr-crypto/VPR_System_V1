c070ad6e84f936e77f2cd973f3ffae25ce2f3ae6
DeveloperBotivate <gitbotivate@gmail.com>
2026-09-01 15:54:43 +0530

    case sencitive

--- src/components/AmbientBackground.jsx
+++ src/components/AmbientBackground.jsx
@@ -7,6 +7,7 @@ import React from 'react';
  */
 const AmbientBackground = () => {
   return (
+
     <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden bg-white">
       <div className="absolute -top-40 -left-32 h-[32rem] w-[32rem] rounded-full bg-royal-100/60 blur-[120px]" />
       <div className="absolute top-1/3 -right-40 h-[36rem] w-[36rem] rounded-full bg-sky-100/70 blur-[130px] animate-aidash-float" />
